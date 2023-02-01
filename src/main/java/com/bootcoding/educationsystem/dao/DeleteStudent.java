32ee84e5719164b1270525d76f14cd88899f7186
Anshul0707 <anshul.markwade786@gmail.com>
2023-02-01 14:40:31 +0530

    code implemented

--- src/main/java/com/bootcoding/educationsystem/dao/DeleteStudent.java
+++ src/main/java/com/bootcoding/educationsystem/dao/DeleteStudent.java
@@ -6,14 +6,29 @@ public class DeleteStudent
 {
     public static void main(String[] args)
     {
+        int [] id={16,17};
+        deletRecord(id);
+    }
+    public static void deletRecord(int [] id)
+    {
         Connection con= DbConnection.getConnection();
+
+        String s = "";
+        for (int i = 0; i < id.length; i++)
+        {
+            s  = s + id[i];
+            if(i < id.length-1)
+            {
+                s = s + ",";
+            }
+        }
+
         try
         {
-           String query="DELETE FROM student " +
-            "WHERE id = 7";
-           Statement smt = con.createStatement();
-           smt.executeUpdate(query);
-           System.out.println("Data inserted successfully..");
+            String query="DELETE FROM student WHERE id IN ("+ s +")";
+            Statement smt = con.createStatement();
+            smt.executeUpdate(query);
+            System.out.println("Data deleted successfully..");
         }
         catch (Exception ex)
         {
